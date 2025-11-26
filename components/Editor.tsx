386a6c3a7de1e1296d2f92e57b32b0c9062c317e
niikkhilsharma <niikkhilsharma@gmail.com>
2025-11-26 22:23:01 +0530

    fixed build error

--- components/Editor.tsx
+++ components/Editor.tsx
@@ -31,7 +31,8 @@ const ImagesPlugin = dynamic(() => import('@/plugins/ImagePlugin/index.jsx'), {
 	ssr: false,
 })
 
-const TableHoverActionsPlugin = dynamic(() => import('@/plugins/TableHoverActionsPlugin/index.jsx'), {
+// const TableHoverActionsPlugin = dynamic(() => import('@/plugins/TableHoverActionsPlugin/index.jsx'), {
+const TableHoverActionsPlugin = dynamic(() => import('../Plugins/TableHoverActionsPlugin/index.jsx'), {
 	ssr: false,
 })
 
